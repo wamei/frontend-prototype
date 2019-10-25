f334b45c0d7ca65989030ca37f28633a5ac89f66
wamei <wamei.cho@gmail.com>
2019-10-25 13:57:22 +0900

    add settings for github pages

---
+++ docs/precache-manifest.377bcac6db3cbe3dedaf9df54aa8cdae.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22b10349b13325457b3",
+    "url": "/frontend-prototype/css/app.698b5943.css"
+  },
+  {
+    "revision": "276c7ef0a511b3f0187c",
+    "url": "/frontend-prototype/css/chunk-2cac339a.007a06f7.css"
+  },
+  {
+    "revision": "9ef588b9dc65433c08fc",
+    "url": "/frontend-prototype/css/chunk-6d65bf76.007a06f7.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/frontend-prototype/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4e1e7f76d09c2f81cbb6b31807418a87",
+    "url": "/frontend-prototype/index.html"
+  },
+  {
+    "revision": "f22b10349b13325457b3",
+    "url": "/frontend-prototype/js/app.b0761e79.js"
+  },
+  {
+    "revision": "276c7ef0a511b3f0187c",
+    "url": "/frontend-prototype/js/chunk-2cac339a.8493e4a6.js"
+  },
+  {
+    "revision": "9ef588b9dc65433c08fc",
+    "url": "/frontend-prototype/js/chunk-6d65bf76.63c819be.js"
+  },
+  {
+    "revision": "88fce376339d6eae8fc1",
+    "url": "/frontend-prototype/js/chunk-712f2df5.174e167a.js"
+  },
+  {
+    "revision": "adf8c4834ac7854c3355",
+    "url": "/frontend-prototype/js/chunk-vendors.fd69a162.js"
+  },
+  {
+    "revision": "90c30de33597919bf3aeab19726516c4",
+    "url": "/frontend-prototype/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/frontend-prototype/robots.txt"
+  }
+]);
